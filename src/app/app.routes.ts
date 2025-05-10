0f2ddadd97133d79d67cc5583c7a390d45bfebb6
Williams <avendano.balarezo.williams@gmail.com>
2025-05-10 10:22:58 -0500

    feat(subscription): add subscription bounded context.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { SubscriptionOverviewComponent } from './subscription/pages/subscription-overview/subscription-overview.component';
+import { DashboardLayoutComponent } from './public/layout/dashboard-layout/dashboard-layout.component';
+import { SummaryOverviewComponent } from './summary/pages/summary-overview/summary-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', redirectTo: '/dashboard/summary', pathMatch: 'full' },
+    {
+        path: 'dashboard', component: DashboardLayoutComponent,
+        children: [
+            { path: '', redirectTo: 'summary', pathMatch: 'full' }, // 👈 redirección interna
+            { path: 'summary', component: SummaryOverviewComponent },
+            { path: 'subscription', component: SubscriptionOverviewComponent },
+        ]
+    },
+    { path: '**', redirectTo: '/dashboard/summary' } // ruta comodín para errores 404
+
+
+
+];
