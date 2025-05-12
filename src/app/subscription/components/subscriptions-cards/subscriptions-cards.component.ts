0e103720ab6b12de366fa506bf393de319caa6e3
Williams <avendano.balarezo.williams@gmail.com>
2025-05-11 23:02:59 -0500

    chore(json-server): add db.json and routes

--- src/app/subscription/components/subscriptions-cards/subscriptions-cards.component.ts
+++ src/app/subscription/components/subscriptions-cards/subscriptions-cards.component.ts
@@ -1,8 +1,9 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from '../../model/subscription.entity';
 
 @Component({
   selector: 'app-subscriptions-cards',
@@ -16,43 +17,8 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './subscriptions-cards.component.css'
 })
 export class SubscriptionsCardsComponent {
-  //plans es arreglo de objetos
-  plans = [
-    {
-      name: 'Monthly plan',
-      price: 'S/. 59.99 / month',
-      features: [
-        'Automated inventory management',
-        'Ordering and purchasing control',
-        'Reporting and analytics ',
-        'Critical stock notifications',
-        'Integration with suppliers'
-      ],
-      popular: false
-    },
-    {
-      name: 'Anual plan',
-      price: 'S/. 39.99 / month',
-      features: [
-        'Gestión de inventario automatizado',
-        'Control de pedidos y compras',
-        'Reporte y analítica',
-        'Notificaciones de stock crítico',
-        'Integración con proveedores'
-      ],
-      popular: true
-    },
-    {
-      name: 'Semiannual Plan',
-      price: 'S/. 49.99 / month',
-      features: [
-        'Automated inventory management',
-        'Ordering and purchasing control',
-        'Reporting and analytics',
-        'Critical stock notifications',
-        'Integration with suppliers'
-      ],
-      popular: false
-    }
-  ];
+
+  @Input() subscriptions: Array<Subscription> = [];
+
+
 }
