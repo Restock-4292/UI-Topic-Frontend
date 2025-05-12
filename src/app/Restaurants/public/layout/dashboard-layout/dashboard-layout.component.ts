8fde40e836dca929a0657837ee910bd9bf1eb5f3
Williams <avendano.balarezo.williams@gmail.com>
2025-05-12 17:09:00 -0500

    feat(subscriptions): organize code

--- src/app/Restaurants/public/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/Restaurants/public/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
+import { SidebarComponent } from '../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -14,5 +14,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './dashboard-layout.component.css'
 })
 export class RestaurantDashboardLayoutComponent {
+  menu = [
+    { labelKey: 'sidebar.summary', icon: 'bar_chart', route: '/dashboard/restaurant/summary' },
+    { labelKey: 'sidebar.subscription', icon: 'credit_card', route: '/dashboard/restaurant/subscription' },
+    { labelKey: 'sidebar.inventory', icon: 'inventory_2', route: '/dashboard/restaurant/inventory' },
+    { labelKey: 'sidebar.suppliers', icon: 'groups', route: '/dashboard/restaurant/suppliers' },
+    { labelKey: 'sidebar.alerts', icon: 'notifications', route: '/dashboard/restaurant/alerts' },
+    { labelKey: 'sidebar.orders', icon: 'local_shipping', route: '/dashboard/restaurant/orders' },
+    { labelKey: 'sidebar.recipes', icon: 'restaurant_menu', route: '/dashboard/restaurant/recipes' },
+    { labelKey: 'sidebar.sales', icon: 'room_service', route: '/dashboard/restaurant/sales' },
+  ];
 
 }
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatNavList } from "@angular/material/list";
+import { RouterLink } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
+import { TranslatePipe } from "@ngx-translate/core";
+import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
+import { User, UserService } from '../../../services/user.service';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [
+    MatSidenavModule,
+    MatNavList,
+    RouterLink,
+    MatIcon,
+    LanguageSwitcherComponent,
+    TranslatePipe
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  @Input() menu: Array<any> = [];
+
+
+
+  user!: User;
+
+  constructor(private userService: UserService) {
+    this.userService.getUser().subscribe(data => {
+      this.user = data;
+    });
+  }
+}
