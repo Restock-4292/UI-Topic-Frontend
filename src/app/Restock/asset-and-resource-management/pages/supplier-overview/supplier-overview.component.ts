a117d8ff76e63311bda4ac9439cce157ef5ba841
Yaku Guzman <yakumgc12@gmail.com>
2025-05-13 16:55:29 -0500

    feat(supplier-overview): create suppliers overview component.

---
+++ src/app/Restock/asset-and-resource-management/pages/supplier-overview/supplier-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-overview',
+  imports: [],
+  templateUrl: './supplier-overview.component.html',
+  styleUrl: './supplier-overview.component.css'
+})
+export class SupplierOverviewComponent {
+
+}
