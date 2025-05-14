a149e49f8694695957bbf2fbc2a8f5fce3401548
Yaku Guzman <yakumgc12@gmail.com>
2025-05-14 16:20:05 -0500

    feat(summary-alerts): add summary alerts chore.

---
+++ src/app/Restock/Analytics/components/supplier-alerts-widget/supplier-alerts-widget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-alerts-widget',
+  imports: [],
+  templateUrl: './supplier-alerts-widget.component.html',
+  styleUrl: './supplier-alerts-widget.component.css'
+})
+export class SupplierAlertsWidgetComponent {
+
+}
