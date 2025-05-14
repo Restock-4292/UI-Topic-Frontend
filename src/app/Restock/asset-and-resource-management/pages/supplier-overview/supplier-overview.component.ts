3adad0369c1df47c73b370fd7b545acd9afb53b8
Yaku Guzman <yakumgc12@gmail.com>
2025-05-13 19:15:53 -0500

    feat(supplier-overview): add supplier overview component.

--- src/app/Restock/asset-and-resource-management/pages/supplier-overview/supplier-overview.component.ts
+++ src/app/Restock/asset-and-resource-management/pages/supplier-overview/supplier-overview.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SupplierModalComponent} from '../../components/supplier-modal/supplier-modal.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-supplier-overview',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    SupplierModalComponent
+  ],
   templateUrl: './supplier-overview.component.html',
   styleUrl: './supplier-overview.component.css'
 })
-export class SupplierOverviewComponent {
+export class SupplierOverviewComponent implements OnInit {
+  showAddSupplierModal = false;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.showAddSupplierModal = params['addSupplier'] === 'true';
+    });
+  }
+
+  openAddSupplierModal(): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { addSupplier: true },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  closeModal(): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { addSupplier: null },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  viewCatalog(supplier: any): void {
+    this.router.navigate(['/dashboard/suppliers', supplier.id]);
+  }
 }
