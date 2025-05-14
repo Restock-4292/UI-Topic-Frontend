975ec357fd9fad7b817bb9f38564efbbaa4df188
Yaku Guzman <yakumgc12@gmail.com>
2025-05-13 19:11:04 -0500

    feat(supplier): add supplier entity.

---
+++ src/app/Restock/asset-and-resource-management/model/supplier.entity.ts
@@ -0,0 +1,42 @@
+export class Supplier {
+  id: number;
+  name: string;
+  ruc: string;
+  category: string;
+  status: boolean;
+  registrationDate: string;
+  lastUpdate: string;
+  email: string;
+  phone: string;
+  address: string;
+  contactPerson: string;
+  position: string;
+
+  constructor(supplier: {
+    id?: number,
+    name?: string,
+    ruc?: string,
+    category?: string,
+    status?: boolean,
+    registrationDate?: string,
+    lastUpdate?: string,
+    email?: string,
+    phone?: string,
+    address?: string,
+    contactPerson?: string,
+    position?: string
+  }) {
+    this.id = supplier.id || 0;
+    this.name = supplier.name || '';
+    this.ruc = supplier.ruc || '';
+    this.category = supplier.category || '';
+    this.status = supplier.status ?? false;
+    this.registrationDate = supplier.registrationDate || '';
+    this.lastUpdate = supplier.lastUpdate || '';
+    this.email = supplier.email || '';
+    this.phone = supplier.phone || '';
+    this.address = supplier.address || '';
+    this.contactPerson = supplier.contactPerson || '';
+    this.position = supplier.position || '';
+  }
+}
