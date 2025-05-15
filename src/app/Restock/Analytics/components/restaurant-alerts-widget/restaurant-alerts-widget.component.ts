04e0e0f5a6df17bf13ada493b20208874e29d120
Yaku Guzman <yakumgc12@gmail.com>
2025-05-14 21:51:50 -0500

    feat(restaurant-analytics-alert): restaurant analytics alerts component.

---
+++ src/app/Restock/Analytics/components/restaurant-alerts-widget/restaurant-alerts-widget.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-restaurant-alerts-widget',
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatCellDef,
+    MatCell,
+    MatButton,
+    MatRow,
+    MatHeaderRowDef,
+    MatHeaderRow,
+    MatRowDef
+  ],
+  templateUrl: './restaurant-alerts-widget.component.html',
+  styleUrl: './restaurant-alerts-widget.component.css'
+})
+export class RestaurantAlertsWidgetComponent {
+  displayedColumns: string[] = ['ingredient', 'status'];
+
+  alerts = [
+    { ingredient: 'Aji amarillo', status: 'Low stock' },
+    { ingredient: 'Cebolla roja', status: 'Low stock' },
+    { ingredient: 'Culantro', status: 'Maximum stock' },
+    { ingredient: 'Lechuga', status: 'Low stock' }
+  ];
+
+  get topThree() {
+    return this.alerts.slice(0, 4);
+  }
+}
