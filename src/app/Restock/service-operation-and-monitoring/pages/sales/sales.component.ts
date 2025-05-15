16e141009ef171ee0ff86a142a36af4752714cf4
Williams <avendano.balarezo.williams@gmail.com>
2025-05-14 21:13:46 -0500

    feat(sales): adding sales table

--- src/app/Restock/service-operation-and-monitoring/pages/sales/sales.component.ts
+++ src/app/Restock/service-operation-and-monitoring/pages/sales/sales.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
@@ -6,7 +6,16 @@ import { RegisterSalesComponent } from '../../components/register-sales/register
 import { CommonModule } from '@angular/common';
 import { SaleConfirmationComponent } from '../../components/sale-confirmation/sale-confirmation.component';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
+interface Orden {
+  fecha: string;
+  platos: any[];
+  insumos: any[];
+}
 @Component({
   selector: 'app-sales',
   imports: [
@@ -15,7 +24,11 @@ import { ActivatedRoute, Router } from '@angular/router';
     CommonModule,
     MatDividerModule,
     RegisterSalesComponent,
-    SaleConfirmationComponent
+    SaleConfirmationComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   templateUrl: './sales.component.html',
   styleUrl: './sales.component.css'
@@ -23,8 +36,40 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 
 export class SalesComponent implements OnInit {
-  mostrarRegistroVenta = false; //muestra el modal de registro de un comensal
-  mostrarSaleConfirmation = false; //muestra modal de confimracion de venta realizada
+  //Tables of history sales added in inventory
+  displayedColumns: string[] = ['code', 'plates', 'additonal_supplies', 'actions'];
+  dataSource = new MatTableDataSource<any>(
+    [
+      { code: "C7Y5ND2", quantity_plates: 2, quantity_additonal_supplies: 4 },
+      { code: "AQWE4TG", quantity_plates: 3, quantity_additonal_supplies: 8 },
+      { code: "ZSWEDC5", quantity_plates: 3, quantity_additonal_supplies: 4 },
+      { code: "CV8ESXD", quantity_plates: 6, quantity_additonal_supplies: 6 },
+      { code: "BJIWS52", quantity_plates: 5, quantity_additonal_supplies: 2 },
+      { code: "ABVDTB1", quantity_plates: 3, quantity_additonal_supplies: 7 },
+      { code: "BUDCS19", quantity_plates: 11, quantity_additonal_supplies: 4 },
+    ]);
+
+  //Paginator of table
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  //Apllying filter for searching by code
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+
+  //Show the register Modal: register a sale
+  mostrarRegistroVenta = false;
+  //Show Confirmation Modal of sale completed
+  mostrarSaleConfirmation = false;
+  // <-- Array that contains orders, this orders will show in a modal component
+  sales: Orden[] = [];
+  salesAddedToInventory: Orden[] = [];
 
   constructor(
     private router: Router,
@@ -32,11 +77,13 @@ export class SalesComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    //show the register modal if the query from the url registerSale is true
     this.route.queryParams.subscribe(params => {
       this.mostrarRegistroVenta = params['registerSale'] === 'true';
     });
   }
 
+  // show the register modal and put the registerSale ==true
   toggleRegistroVenta() {
     this.router.navigate([], {
       relativeTo: this.route,
@@ -45,6 +92,7 @@ export class SalesComponent implements OnInit {
     });
   }
 
+  //close the modal: query registerSale == false
   closeModal(): void {
     this.router.navigate([], {
       relativeTo: this.route,
@@ -53,12 +101,24 @@ export class SalesComponent implements OnInit {
     });
   }
 
+  //Register a sale 
   onRegisterSale(data: { platos: any[]; insumos: any[] }) {
-    // Aquí puedes hacer fetch si necesitas, y luego abrir el modal
-    console.log("data de registerSale: ", data);
-    //si la respuesta del fecth es exitosa entonces se abre modal de confirmacion
-    // Abre modal de confirmación
+
+    //Simulating creating a sale in backend
+    const newSale: Orden = {
+      fecha: new Date().toISOString(),
+      platos: data.platos,
+      insumos: data.insumos
+    };
+
+    //Simulando tener las ordenes ya registradas en base de datos
+    this.sales.push(newSale);
+
+    // if the order was created succesfully ,then show saleConfirmation modal
     this.mostrarSaleConfirmation = true;
   }
+
+  // Check on the backend whether any sales records have the 'AddedInInventory' field set
+  showHistorySalesAddedinInventory = true;
 
 }
