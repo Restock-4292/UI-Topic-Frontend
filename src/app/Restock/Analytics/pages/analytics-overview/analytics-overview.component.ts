cd81849cfe4e7bd09d96648467e3e67aa8d8b9fa
Yaku Guzman <yakumgc12@gmail.com>
2025-05-14 19:19:14 -0500

    feat(supplier-analytics-overview): add supplier analytics overview page.

--- src/app/Restock/Analytics/pages/analytics-overview/analytics-overview.component.ts
+++ src/app/Restock/Analytics/pages/analytics-overview/analytics-overview.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {SupplierAlertsWidgetComponent} from '../../components/supplier-alerts-widget/supplier-alerts-widget.component';
+import {
+  SupplierAccountWidgetComponent
+} from '../../components/supplier-account-widget/supplier-account-widget.component';
 
 @Component({
   selector: 'app-analytics-overview',
-  imports: [],
+  imports: [
+    SupplierAlertsWidgetComponent,
+    SupplierAccountWidgetComponent
+  ],
   templateUrl: './analytics-overview.component.html',
   styleUrl: './analytics-overview.component.css'
 })
