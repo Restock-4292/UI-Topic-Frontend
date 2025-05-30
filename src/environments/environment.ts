32dde2862bdea428e9e88ecd9203c3e3ab4f42c9
jahazielgg <u202319831@upc.edu.pe>
2025-05-30 18:17:18 -0500

    feat(environment): add new API endpoints for recipes, supplies, categories, and unit measurements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,11 @@
 export const environment = {
-    production: true,
-    serverBaseUrl: 'http://localhost:3000',
-    subscriptionsEndpointPath: '/subscriptions',
-    suppliersEndpointPath: '/users'
+  production: true,
+  serverBaseUrl: 'http://localhost:3000',
+  subscriptionsEndpointPath: '/subscriptions',
+  suppliersEndpointPath: '/users',
+  recipesEndpointPath: '/recipes',
+  suppliesEndpointPath: '/supplies',
+  recipeSuppliesEndpointPath: '/recipes_supplies',
+  categoriesEndpointPath: '/categories',
+  unitMeasurementsEndpointPath: '/units_measurements',
 };
