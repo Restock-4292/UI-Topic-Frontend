7ed1641f2ff3bc3eb680c9f319a5ed46510e48af
jahazielgg <u202319831@upc.edu.pe>
2025-06-01 09:46:55 -0500

    feat(delete): implement delete component with confirmation dialog and styling

---
+++ src/app/shared/components/delete/delete.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, Output, EventEmitter, Inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-delete',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { label: string },
+    private dialogRef: MatDialogRef<DeleteComponent>
+  ) {}
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+}
