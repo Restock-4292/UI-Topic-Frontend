a10f8ccb7dcb417b5e1440e4b71e3d208250adc4
jahazielgg <u202319831@upc.edu.pe>
2025-06-02 02:17:42 -0500

    feat(supply): create supply form component with handling for submission and cancellation

--- src/app/shared/components/create-and-edit-form/create-and-edit-form.component.ts
+++ src/app/shared/components/create-and-edit-form/create-and-edit-form.component.ts
@@ -15,7 +15,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
-import {MatOptionModule} from '@angular/material/core';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 
 export interface FormFieldSchema {
@@ -40,7 +40,8 @@ export interface FormFieldSchema {
     MatIconModule,
     MatSelectModule,
     MatOptionModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './create-and-edit-form.component.html',
   styleUrls: ['./create-and-edit-form.component.css'],
