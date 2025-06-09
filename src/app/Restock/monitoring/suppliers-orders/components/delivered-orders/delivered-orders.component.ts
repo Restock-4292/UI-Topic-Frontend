d2f8f978cb02dd70e2ecd7899cb385166e456d57
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-09 03:06:26 -0500

    feat(supplier-orders): add delivered orders component.

---
+++ src/app/Restock/monitoring/suppliers-orders/components/delivered-orders/delivered-orders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivered-orders',
+  imports: [],
+  templateUrl: './delivered-orders.component.html',
+  styleUrl: './delivered-orders.component.css'
+})
+export class DeliveredOrdersComponent {
+
+}
