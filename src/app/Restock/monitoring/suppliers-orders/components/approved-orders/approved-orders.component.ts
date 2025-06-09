bd02ad9b17e3435924977e49bec4e2f9a7086361
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-09 03:06:14 -0500

    feat(supplier-orders): add approved orders component.

---
+++ src/app/Restock/monitoring/suppliers-orders/components/approved-orders/approved-orders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approved-orders',
+  imports: [],
+  templateUrl: './approved-orders.component.html',
+  styleUrl: './approved-orders.component.css'
+})
+export class ApprovedOrdersComponent {
+
+}
