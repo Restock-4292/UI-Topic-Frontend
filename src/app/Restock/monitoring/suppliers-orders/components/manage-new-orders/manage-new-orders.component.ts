fdd54d4d227b385dee7e81bd3545b9b67a08b879
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-09 03:07:13 -0500

    feat(supplier-orders): add manage new order component.

---
+++ src/app/Restock/monitoring/suppliers-orders/components/manage-new-orders/manage-new-orders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-new-orders',
+  imports: [],
+  templateUrl: './manage-new-orders.component.html',
+  styleUrl: './manage-new-orders.component.css'
+})
+export class ManageNewOrdersComponent {
+
+}
