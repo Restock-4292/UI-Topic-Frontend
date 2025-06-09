0942e5559cd0115b177e77051ed1a3db3ea50978
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-09 03:06:57 -0500

    feat(supplier-orders): add filter section component.

---
+++ src/app/Restock/monitoring/suppliers-orders/components/filter-section/filter-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-section',
+  imports: [],
+  templateUrl: './filter-section.component.html',
+  styleUrl: './filter-section.component.css'
+})
+export class FilterSectionComponent {
+
+}
