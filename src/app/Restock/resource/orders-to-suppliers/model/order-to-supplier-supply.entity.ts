341d6bb38c218e2bb40d3e7e49d5430dfe26af9f
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-09 23:24:39 -0500

    feat(orders): add order to supplier entity.

---
+++ src/app/Restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.ts
@@ -0,0 +1,13 @@
+export class OrderToSupplierSupply {
+  order_to_supplier_id: number;
+  supply_id: number;
+  quantity: number;
+  accepted: boolean;
+
+  constructor(data: Partial<OrderToSupplierSupply> = {}) {
+    this.order_to_supplier_id = data.order_to_supplier_id || 0;
+    this.supply_id = data.supply_id || 0;
+    this.quantity = data.quantity || 0;
+    this.accepted = data.accepted || false;
+  }
+}
