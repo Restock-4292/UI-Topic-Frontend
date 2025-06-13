a4334b0905d7abb2fe77cbd7e643384a62a777bf
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-13 15:30:37 -0500

    fix(orders): change supply id to batch id of the orders to supplier supply.

--- src/app/Restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.ts
+++ src/app/Restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.ts
@@ -1,12 +1,12 @@
 export class OrderToSupplierSupply {
   order_to_supplier_id: number;
-  supply_id: number;
+  batch_id: number;
   quantity: number;
   accepted: boolean;
 
   constructor(data: Partial<OrderToSupplierSupply> = {}) {
     this.order_to_supplier_id = data.order_to_supplier_id || 0;
-    this.supply_id = data.supply_id || 0;
+    this.batch_id = data.batch_id || 0;
     this.quantity = data.quantity || 0;
     this.accepted = data.accepted || false;
   }
