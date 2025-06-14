cc8f70f5cdcb0459256e53fa2bfb8e243a4b2440
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-14 09:43:23 -0500

    feat(new-orders): add empty section.

--- src/app/Restock/monitoring/suppliers-orders/components/new-orders/new-orders.component.ts
+++ src/app/Restock/monitoring/suppliers-orders/components/new-orders/new-orders.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {FilterSectionComponent} from '../filter-section/filter-section.component';
 import {MatIcon} from '@angular/material/icon';
 import {MatPaginator} from '@angular/material/paginator';
-import {CurrencyPipe} from '@angular/common';
+import {CurrencyPipe, DatePipe} from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatOption} from '@angular/material/core';
 import {MatSelect} from '@angular/material/select';
@@ -13,6 +13,7 @@ import {ManageNewOrdersComponent} from '../manage-new-orders/manage-new-orders.c
 import {OrderToSupplier} from '../../../../resource/orders-to-suppliers/model/order-to-supplier.entity';
 import {Supply} from '../../../../resource/inventory/model/supply.entity';
 import {OrderToSupplierBatch} from '../../../../resource/orders-to-suppliers/model/order-to-supplier-batch.entity';
+import {EmptySectionComponent} from '../../../../../shared/components/empty-section/empty-section.component';
 
 @Component({
   selector: 'app-new-orders',
@@ -28,7 +29,9 @@ import {OrderToSupplierBatch} from '../../../../resource/orders-to-suppliers/mod
     MatIconButton,
     MatButton,
     OrderDetailsComponent,
-    ManageNewOrdersComponent
+    ManageNewOrdersComponent,
+    EmptySectionComponent,
+    DatePipe
   ],
   templateUrl: './new-orders.component.html',
   styleUrl: './new-orders.component.css'
@@ -39,12 +42,22 @@ export class NewOrdersComponent {
   @Input() adminRestaurantsProfiles: { [orderId: number]: string } = {};
 
   @Input() orderSuppliesDetails: { orderId: number; supplies: Supply[] }[] = [];
-  @Input() suppliesGroupedByOrder: { orderId: number; supplies: OrderToSupplierBatch[] }[] = [];
+  @Input() batchesGroupedByOrder: { orderId: number; batches: OrderToSupplierBatch[] }[] = [];
 
+  @Output() manageNewOrderModal = new EventEmitter<OrderToSupplier>();
+  @Output() declineDialog = new EventEmitter<OrderToSupplier>();
 
   displayedColumns: string[] = ['orderDate', 'restaurantName', 'requestedProducts', 'finalPrice', 'actions'];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  openDeclineDialog(order: any): void {
+    this.declineDialog.emit(order);
+  }
+
+  openManageNewOrderModal(order: any): void {
+    this.manageNewOrderModal.emit(order);
+  }
 
   searchTerm: string = '';
   dateRange: string = '';
