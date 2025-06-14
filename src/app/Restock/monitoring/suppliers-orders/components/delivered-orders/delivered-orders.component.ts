57183d8502638e9b8e037331753a4b49a2fce802
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-13 21:55:03 -0500

    feat(orders): add styles and inputs to delivered orders.

--- src/app/Restock/monitoring/suppliers-orders/components/delivered-orders/delivered-orders.component.ts
+++ src/app/Restock/monitoring/suppliers-orders/components/delivered-orders/delivered-orders.component.ts
@@ -1,11 +1,12 @@
-import {Component, ViewChild} from '@angular/core';
-import {CurrencyPipe} from "@angular/common";
+import {Component, Input, ViewChild} from '@angular/core';
+import {CurrencyPipe, DatePipe} from "@angular/common";
 import {FilterSectionComponent} from "../filter-section/filter-section.component";
 import { MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {MatPaginator} from '@angular/material/paginator';
 import {MatIcon} from '@angular/material/icon';
 import {OrderDetailsComponent} from '../order-details/order-details.component';
 import {MatIconButton} from '@angular/material/button';
+import {OrderToSupplier} from '../../../../resource/orders-to-suppliers/model/order-to-supplier.entity';
 
 @Component({
   selector: 'app-delivered-orders',
@@ -16,35 +17,20 @@ import {MatIconButton} from '@angular/material/button';
     MatPaginator,
     MatTableModule,
     OrderDetailsComponent,
-    MatIconButton
+    MatIconButton,
+    DatePipe
   ],
   templateUrl: './delivered-orders.component.html',
   styleUrl: './delivered-orders.component.css'
 })
 export class DeliveredOrdersComponent {
+  @Input() orders: Array<OrderToSupplier> = [];
+  @Input() adminRestaurantsProfiles: { [orderId: number]: string } = {};
+
   displayedColumns: string[] = ['orderDate', 'shipDate', 'restaurantName', 'requestedProducts', 'finalPrice', 'actions'];
-  orders = new MatTableDataSource([
-    {
-      orderDate: '2025-06-08',
-      shipDate: '2025-04-08',
-      restaurantName: 'Pizzería Bella',
-      requestedProducts: 3,
-      finalPrice: 89.90
-    },
-    {
-      orderDate: '2025-04-07',
-      shipDate: '2025-06-07',
-      restaurantName: 'Sushi Express',
-      requestedProducts: 5,
-      finalPrice: 145.50
-    }
-  ]);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngAfterViewInit() {
-    this.orders.paginator = this.paginator;
-  }
 
   showModal = false;
 
@@ -58,5 +44,15 @@ export class DeliveredOrdersComponent {
 
   onModalClose(value: boolean) {
     this.showModal = value;
+  }
+
+  // Method to get CSS class according to the order state
+  getRowClass(order: OrderToSupplier): string {
+    if (!order.state || !order.state.name) return '';
+
+    if (order.state.name.toLowerCase() == 'delivered')
+        return 'row-delivered';
+    else
+      return '';
   }
 }
