e922cf1df5a71fec4d21828db8d6d7f318be2a47
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-17 06:41:18 -0500

    feat(manage-new-orders): add validation.

--- src/app/Restock/monitoring/suppliers-orders/components/manage-new-orders/manage-new-orders.component.ts
+++ src/app/Restock/monitoring/suppliers-orders/components/manage-new-orders/manage-new-orders.component.ts
@@ -80,7 +80,7 @@ export class ManageNewOrdersComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Si hay datos inyectados del modal, usarlos
+
     if (this.injectedData) {
       console.log('Datos inyectados:', this.injectedData);
 
@@ -102,6 +102,14 @@ export class ManageNewOrdersComponent implements OnInit {
 
   onAcceptSelection(): void
   {
+    if (this.selection.selected.length <= 0) {
+      this.snackBar.open('Please select at least one supply to approve the order', 'Close', {
+        duration: 4000,
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
     const updateData = this.buildUpdateData();
     console.log("voy a enviar los datos actualizados:", updateData);
     this.acceptSelection.emit(updateData);
@@ -116,7 +124,7 @@ export class ManageNewOrdersComponent implements OnInit {
     return {
       id: this.order?.id || 0,
       date: this.order?.date || new Date(),
-      description: this.localOrder.description || this.order?.description || '',
+      description: this.localOrder.description || this.order?.description || 'No description provided',
       admin_restaurant_id: this.order?.admin_restaurant_id || 0,
       supplier_id: this.order?.supplier_id || 0,
       order_to_supplier_state_id: onHoldStateId,
