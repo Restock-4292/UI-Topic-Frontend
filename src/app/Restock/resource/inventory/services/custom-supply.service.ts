b535dfc843c8d766dcadd3bd6fd988b30decea02
jahazielgg <u202319831@upc.edu.pe>
2025-07-02 18:05:17 -0500

    feat(inventory): implement custom supply assembler and update service to integrate catalog supplies

--- src/app/Restock/resource/inventory/services/custom-supply.service.ts
+++ src/app/Restock/resource/inventory/services/custom-supply.service.ts
@@ -2,9 +2,10 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError, firstValueFrom, retry, throwError } from 'rxjs';
 import { CategoryService } from './category.service';
-import { UnitMeasurementService } from './unit-measurement.service';
 import { Supply } from '../model/supply.entity';
 import {environment} from '../../../../../environments/environment.development';
+import {CatalogSupplyService} from './catalog-supply.service';
+import {CustomSupplyAssembler} from './custom-supply.assembler';
 
 export interface CustomSupplyPayload {
   supplyId: string;
@@ -19,28 +20,35 @@ export interface CustomSupplyPayload {
 export class CustomSupplyService {
   private http = inject(HttpClient);
   private categories = inject(CategoryService);
-  private units = inject(UnitMeasurementService);
+  private catalog = inject(CatalogSupplyService);
   private baseUrl = environment.serverBaseUrlBackend;
   private endpoint = environment.customSuppliesEndpointPath;
   private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
   async getAll(): Promise<Supply[]> {
-    const [rawSupplies, categories, units] = await Promise.all([
-      firstValueFrom(this.http.get<any[]>(`${this.baseUrl}${this.endpoint}`, this.httpOptions)
-        .pipe(retry(2), catchError(this.handleError))),
-      this.categories.getAllCategories(),
-      this.units.getAllUnitMeasurements()
+    const [customs, catalogSupplies, categories] = await Promise.all([
+      firstValueFrom(
+        this.http.get<any[]>(`${this.baseUrl}${this.endpoint}`, this.httpOptions)
+          .pipe(retry(2), catchError(this.handleError))
+      ),
+      this.catalog.getCatalogSupplies(),
+      this.categories.getAllCategories()
     ]);
 
-    return rawSupplies.map(raw => {
-      const category = categories.find(c => c.id === raw.category_id);
-      const unit = units.find(u => u.id === raw.unit_measurement_id);
-      return Supply.fromPersistence(raw, category, unit);
+    return customs.map(custom => {
+      const catalog = catalogSupplies.find(c => c.id === custom.supplyId
+      );
+      const category = categories.find(c => c.id === (catalog?.category_id ?? catalog?.categoryId));
+      return CustomSupplyAssembler.toEntity(custom, catalog, category);
     });
   }
 
-  async create(payload: CustomSupplyPayload): Promise<any> {
-    const res$ = this.http.post(`${this.baseUrl}${this.endpoint}`, payload, this.httpOptions)
+  async create(payload: CustomSupplyPayload | Supply): Promise<any> {
+    const dto = (payload instanceof Supply)
+      ? CustomSupplyAssembler.toDTO(payload)
+      : payload;
+
+    const res$ = this.http.post(`${this.baseUrl}${this.endpoint}`, dto, this.httpOptions)
       .pipe(retry(2), catchError(this.handleError));
     return firstValueFrom(res$);
   }
