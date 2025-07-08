1365b8bbbd86945e091100e5ea47ec6cce8db186
jahazielgg <u202319831@upc.edu.pe>
2025-07-07 19:59:00 -0500

    feat(environment): update backend server URL for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   serverBaseUrl: 'http://localhost:3000',
-  serverBaseUrlBackend: 'http://localhost:8080/api/v1',
+  serverBaseUrlBackend: 'https://ui-topic-backend-production.up.railway.app/api/v1',
   usersEndpointPath: '/users',
   rolesEndpointPath: '/roles',
   profilesEndpointPath: '/profiles',
